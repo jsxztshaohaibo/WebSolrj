2b21146250df308a6d711faeb7d551d0062327ce
haibo.hao <haibo.hao@becom.com.cn>
2020-07-29 16:26:28 +0800

    增加谷歌 布隆过滤器使用方法

---
+++ src/com/jsxztshaohaibo/TestBloomFilter.java
@@ -0,0 +1,44 @@
+package com.jsxztshaohaibo;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+public class TestBloomFilter {
+
+		// bit 数组大小
+	    private static int size = 10000;
+	    // 布隆过滤器
+	    private static BloomFilter<String> bloomFilter 
+	    = BloomFilter.create(Funnels.stringFunnel(Charset.defaultCharset()), size, 0.001);
+
+	    public static void main(String[] args) {
+	        // 先向布隆过滤器中添加 10000 个url
+	        for (int i = 0; i < size; i++) {
+	            String url = "https://voice.hupu.com/nba/" + i;
+	            bloomFilter.put(url);
+	        }
+	        // 前10000个url不会出现误判
+	        for (int i = 0; i < size; i++) {
+	            String url = "https://voice.hupu.com/nba/" + i;
+	            if (!bloomFilter.mightContain(url)) {
+	                System.out.println("该 url 被采集过了");
+	            }
+	        }
+	        List<String> list = new ArrayList<String>(1000);
+	        // 再向布隆过滤器中添加 2000 个 url ，在这2000 个中就会出现误判了
+	        // 误判的个数为 2000 * fpp
+	        for (int i = size; i < size + 2000; i++) {
+	            String url = "https://voice.hupu.com/nba/" + i;
+	            /***可能存在**/
+	            if (bloomFilter.mightContain(url)) {
+	                list.add(url);
+	            }
+	        }
+	        System.out.println("误判数量：" + list.size());
+	    }
+
+}
